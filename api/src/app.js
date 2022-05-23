b315ad13458fe4dc88bb2fe4629cf89eec6c65ec
jero237 <jeronimobalestra@gmail.com>
2022-05-23 11:42:17 -0300

    Authentication set up

--- api/src/app.js
+++ api/src/app.js
@@ -2,11 +2,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 //Routes
 const auth = require('./routes/auth');
 const main = require('./routes/main');
+const passport = require('passport');
 
-app.use(express.json());
-app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(session({
+    secret: 'n2nQxznYyPt4jCab',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}));
+app.use(cookieParser('n2nQxznYyPt4jCab'));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', auth)
+
+module.exports = app;
--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,9 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const LocalStrategy = require('passport-local');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const User = require('../models/index').sequelize.models.user;
 
+passport.use(new LocalStrategy((email, password, done) => {
+    User.findOne({
+        where: {
+            email
+        }
+    }).then(user => {
+        if (!user) return done(null, false, { message: 'Incorrect email or password' })
+        bcrypt.compare(password, user.password, (err, res) => {
+            if (res) return done(null, user)
+            return done(null, false, { message: 'Incorrect email or password' })
+        });
+    });
+}))
 
-router.get('/login', (req, res) => {
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
 
+passport.deserializeUser((id, done) => {
+    User.findByPk(id).then(user => done(null, user))
+});
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return next(err)
+        if (!user) return res.status(401).json(info)
+        req.logIn(user, (err) => {
+            if (err) return next(err)
+            return res.send(user)
+        });
+    })(req, res, next)
+})
+
+router.post('/register', (req, res, next) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(user => {
+        if (user) return res.status(401).json({ message: 'Username already exists' })
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+                return next(err)
+            }
+            User.create({
+                email: req.body.email,
+                password: hash,
+                name: req.body.name
+            }).then(user => {
+                return res.send(user)
+            });
+        });
+    });
+})
+
+router.post('/logout', (req, res, next) => {
+    req.logout(err => {
+        if (err) return next(err)
+        res.send({ message: 'Logged out' })
+    });
+})
+
+router.get('/test', (req, res) => {
+    res.send({ message: req.user })
 })
 
 module.exports = router;
