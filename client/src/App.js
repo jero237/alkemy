7d9dbec06e4816f50976241a0fe62ad75afc1f88
jero237 <jeronimobalestra@gmail.com>
2022-05-27 12:17:17 -0300

    Auto dark mode added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,39 @@
+import { useMemo } from 'react';
+import { ThemeProvider, createTheme, CssBaseline, useMediaQuery } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 function App() {
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="dashboard/*" element={<Dashboard />} />
       </Routes>
-    </>
+    </ThemeProvider>
   )
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { CssBaseline, TextField, Avatar, Button, Link, Box, Typography, Container } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import axios from 'axios';
@@ -48,67 +48,65 @@ export default function SignIn() {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" sx={{ mt: 1 }} >
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            label="Email Address"
-                            name="username"
-                            autoComplete="email"
-                            value={form.username}
-                            onChange={handleChange}
-                            autoFocus
-                            error={(error && !form.username) || (error && !!errorMsg)}
-                            helperText={error && errorMsg}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            value={form.password}
-                            onChange={handleChange}
-                            error={(error && !form.password) || (error && !!errorMsg)}
-                            helperText={error && errorMsg}
-                        />
-                        <Button
-                            fullWidth
-                            variant="contained"
-                            onClick={handleSubmit}
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Box component="form" sx={{ mt: 1 }} >
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        label="Email Address"
+                        name="username"
+                        autoComplete="email"
+                        value={form.username}
+                        onChange={handleChange}
+                        autoFocus
+                        error={(error && !form.username) || (error && !!errorMsg)}
+                        helperText={error && errorMsg}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={form.password}
+                        onChange={handleChange}
+                        error={(error && !form.password) || (error && !!errorMsg)}
+                        helperText={error && errorMsg}
+                    />
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        onClick={handleSubmit}
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
 
-                        <Link href="/register" variant="body2">
-                            {"Don't have an account? Sign Up"}
-                        </Link>
-                    </Box>
-                    <Button endIcon={<GitHubIcon />} color="inherit" href="https://github.com/jero237">jero237</Button>
+                    <Link href="/register" variant="body2">
+                        {"Don't have an account? Sign Up"}
+                    </Link>
                 </Box>
-            </Container>
-        </ThemeProvider>
+                <Button endIcon={<GitHubIcon />} color="inherit" href="https://github.com/jero237">jero237</Button>
+            </Box>
+        </Container>
     );
 }
