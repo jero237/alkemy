c4211e5e1042bcdb7901fe4e52d750b0381519b4
jero237 <jeronimobalestra@gmail.com>
2022-05-27 11:04:35 -0300

    ManageTransactions component created

---
+++ client/src/components/ManageTransactions.jsx
@@ -0,0 +1,18 @@
+import { Container } from "@mui/material"
+import TransactionForm from "./TransactionForm"
+import TransactionTable from "./TransactionTable"
+
+export default function ManageTransactions({ transactions, reloadTransactions }) {
+    return (
+        <Container sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '1rem'
+        }}>
+            <TransactionForm reloadTransactions={reloadTransactions} />
+            <TransactionTable transactions={transactions} />
+        </Container>
+    )
+}
